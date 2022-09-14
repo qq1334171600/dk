5d5404ad49d558591b100c09042affaab183e4d3
zj <1334171600@qq.com>
2022-09-14 22:11:52 +0800

    添加项目文件。

---
+++ HelloWorld/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Device.Location;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HelloWorld
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("打卡成功");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            CLocation myLocation = new CLocation();
+            myLocation.GetLocationEvent();
+        }
+    }
+    class CLocation
+    {
+        GeoCoordinateWatcher watcher;
+
+        public void GetLocationEvent()
+        {
+            this.watcher = new GeoCoordinateWatcher();
+            this.watcher.PositionChanged += new EventHandler<GeoPositionChangedEventArgs<GeoCoordinate>>(watcher_PositionChanged);
+            bool started = this.watcher.TryStart(false, TimeSpan.FromMilliseconds(1000));
+            if (!started)
+            {
+                MessageBox.Show("GeoCoordinateWatcher timed out on start.");
+            }
+        }
+
+        void watcher_PositionChanged(object sender, GeoPositionChangedEventArgs<GeoCoordinate> e)
+        {
+            MessageBox.Show("watcher_PositionChanged/" + e.ToString());
+            PrintPosition(e.Position.Location.Latitude, e.Position.Location.Longitude);
+        }
+
+        void PrintPosition(double Latitude, double Longitude)
+        {
+            MessageBox.Show("Latitude: "+Latitude+", Longitude "+Longitude);
+        }
+    }
+
+}
